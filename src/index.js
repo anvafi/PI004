8636a8347bad6655b8f49a704315b182cffedc16
anvafi <anvafi@alumnatflorida.es>
2024-12-20 22:45:46 +0100

    routeando

--- src/index.js
+++ src/index.js
@@ -5,13 +5,8 @@ app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), leerPuerto());
 
-app.get("/rutaGet", function (req, res) {
-  console.log(
-    "hola desde la consola de node, desde una llamada en el servidor"
-  );
-  res.send("hola desde una web llamada en el servidor");
-});
-
 function leerPuerto() {
-  console.log("hola desde el puerto " + app.get("port"));
+  console.log(`hola desde el puerto: ${app.get("port")}`);
 }
+//super rutas aquí
+app.use(require("./routes/index"));
